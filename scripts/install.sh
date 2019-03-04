d1b5fe5fc6ab29ea17367b8bade190bd914f20a4
hallowf <alexandre.cortegaca@gmail.com>
2019-03-04 20:16:37 +0000

    Build scripts and config

---
+++ scripts/install.sh
@@ -0,0 +1,13 @@
+if [[ "$TRAVIS_OS_NAME" != "windows" ]]; then
+    bash miniconda.sh -b -p $HOME/miniconda;
+    export PATH="$HOME/miniconda:$HOME/miniconda/bin:$PATH";
+    hash -r;
+    conda config --set always_yes yes --set changeps1 no;
+    conda update -q conda;
+    echo $TRAVIS_OS_NAME;
+    echo $TRAVIS_PYTHON_VERSION;
+    conda env create python=$TRAVIS_PYTHON_VERSION -f environment.yml;
+    source activate pybcli;
+  elif  [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
+    $PY -m pip install -r requirements.txt
+  fi
