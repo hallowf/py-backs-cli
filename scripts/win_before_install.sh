ef33ae040e2ab468589cf00be9a6d247a8c35702
hallowf <alexandre.cortegaca@gmail.com>
2019-03-04 21:13:41 +0000

    Fixing python version from chocolatey

--- scripts/win_before_install.sh
+++ scripts/win_before_install.sh
@@ -1,4 +1,4 @@
-choco install python &&
+choco install python3 &&
 mkdir UPX && cd UPX && export UPX_PATH=$(PWD) &&
 curl https://github.com/upx/upx/releases/download/v3.95/upx-3.95-win64.zip -J -L --output UPX.zip &&
 7z e -y UPX.zip -o"." upx-3.95-win64\*.exe &&
