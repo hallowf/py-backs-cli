0db77b409c147506c085f0402a5ba5aae98e1357
hallowf <alexandre.cortegaca@gmail.com>
2019-03-04 00:27:47 +0000

    Added script for pyinstaller

---
+++ pyinstaller.py
@@ -0,0 +1,27 @@
+import PyInstaller.__main__
+import os, shutil
+
+UPX_PATH = os.environ["UPX_PATH"]
+CWD = os.path.dirname(os.path.abspath(__file__))
+
+
+PyInstaller.__main__.run([
+        '--noconfirm',
+        '--upx-dir=%s' % (UPX_PATH),
+        '--log-level=WARN',
+        '--onefile',
+        '--name=PYBCLI',
+        "BackupCLI\\cli\\main.py",
+    ])
+
+
+# Add other to dist
+to_add = {
+    "backups.ini.template": "BackupCli\\cli\\backups.ini.template",
+}
+t_dest = "%s\\dist\\" % (CWD)
+
+for f in to_add:
+    f_src = "%s\\%s" % (CWD, to_add[f])
+    f_dest = t_dest + f
+    shutil.copyfile(f_src, f_dest)
