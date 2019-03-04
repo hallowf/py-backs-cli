a7d79f19c7374b8fad6a058f5f8b9f1399500013
hallowf <alexandre.cortegaca@gmail.com>
2019-03-04 20:55:07 +0000

    Updated scripts and travis config

---
+++ scripts/win_before_install.sh
@@ -0,0 +1,5 @@
+choco install python &&
+mkdir UPX && cd UPX && export UPX_PATH=$(PWD) &&
+curl https://github.com/upx/upx/releases/download/v3.95/upx-3.95-win64.zip -J -L --output UPX.zip &&
+7z e -y UPX.zip -o"." upx-3.95-win64\*.exe &&
+cd ..
